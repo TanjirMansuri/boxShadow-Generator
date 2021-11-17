4e2f3d9bc430645cb8c55a510d598c137e26db1a
tanjir <tanjirmansuri123@gmail.com>
2021-11-18 00:53:35 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [hori, setHori] = useState(10);
+  const [ver, setVer] = useState(10);
+  const [blur, setBlur] = useState(10);
+  const [clr, setClr] = useState("#000000");
+  const [chkOn, setChkOn] = useState(false);
+
+  const hex2rgba = (hex, alpha = 1) => {
+    const [r, g, b] = hex.match(/\w\w/g).map((x) => parseInt(x, 16));
+    return `rgba(${r},${g},${b},${alpha})`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="heading">
+        <h1>You Can Create box-shadow And Copy CSS Code</h1>
+      </div>
+      <div className="App">
+        <div className="controls">
+          <label>Horizontal Lenght</label>
+          <input type="range" value={hori} onChange={(e) => setHori(e.target.value)} min="-200" max="200" />
+
+          <label>Vertical Lenght</label>
+          <input type="range" value={ver} onChange={(e) => setVer(e.target.value)} min="-200" max="200" />
+
+          <label>Blur </label>
+          <input type="range" value={blur} onChange={(e) => setBlur(e.target.value)} min="0" max="200" />
+
+          <label>Color </label>
+          <input type="color" value={clr} onChange={(e) => setClr(e.target.value)} />
+
+          <div className="switch">
+            <label>
+              Outline
+              <input type="checkbox" checked={chkOn} onChange={() => setChkOn(!chkOn)} />
+              <span className="lever"></span>
+              Inset
+            </label>
+          </div>
+        </div>
+        <div className="output">
+          <div className="box" style={{ boxShadow: `${chkOn ? "inset" : ""} ${hori}px ${ver}px ${blur}px ${hex2rgba(clr, 0.5)}` }}>
+            <p>
+              box-shadow:{hori}px {ver}px {blur}px {hex2rgba(clr, 0.5)}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
